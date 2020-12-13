3de9b37da4bba7103a8ab30661865bc66e166bf6
trisct <29689546+trisct@users.noreply.github.com>
2020-12-13 11:11:53 +0000

    modified:   scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,9 +2,9 @@
 set -ex
 
 # Training
-GPU_ID=0
+GPU_ID=2
 DISPLAY_ID=$((GPU_ID*10+10))
-NAME='pifu_demo'
+NAME='pifu_90'
 
 # Network configuration
 
@@ -16,8 +16,8 @@ MLP_DIM_COLOR='513 1024 512 256 128 3'
 # NOTE: one can change here to reconstruct mesh in a different resolution.
 VOL_RES=256
 
-CHECKPOINTS_NETG_PATH='./checkpoints/net_G'
-CHECKPOINTS_NETC_PATH='./checkpoints/net_C'
+CHECKPOINTS_NETG_PATH='./bike_90/example/netG_epoch_40'
+CHECKPOINTS_NETC_PATH='./checkpoints/example/netC_epoch_0'
 
 TEST_FOLDER_PATH='./sample_images'
 
@@ -35,4 +35,4 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python ./apps/eval.py \
     --norm_color 'group' \
     --test_folder_path ${TEST_FOLDER_PATH} \
     --load_netG_checkpoint_path ${CHECKPOINTS_NETG_PATH} \
-    --load_netC_checkpoint_path ${CHECKPOINTS_NETC_PATH}
+    #--load_netC_checkpoint_path ${CHECKPOINTS_NETC_PATH}
