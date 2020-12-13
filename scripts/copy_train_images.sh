01a66df27213e30408589c7da07be720074871c3
trisct <29689546+trisct@users.noreply.github.com>
2020-12-13 11:12:18 +0000

    new file:   scripts/copy_train_images.sh

---
+++ scripts/copy_train_images.sh
@@ -0,0 +1,28 @@
+cp ../training_data/RENDER/bike_1/45_0_00.jpg ../sample_images/1_45.jpg
+cp ../training_data/RENDER/bike_2/45_0_00.jpg ../sample_images/2_45.jpg
+cp ../training_data/RENDER/bike_3/45_0_00.jpg ../sample_images/3_45.jpg
+cp ../training_data/RENDER/bike_5/45_0_00.jpg ../sample_images/5_45.jpg
+cp ../training_data/RENDER/bike_7/45_0_00.jpg ../sample_images/7_45.jpg
+cp ../training_data/RENDER/bike_9/45_0_00.jpg ../sample_images/9_45.jpg
+
+cp ../training_data/RENDER/bike_1/90_0_00.jpg ../sample_images/1_90.jpg
+cp ../training_data/RENDER/bike_2/90_0_00.jpg ../sample_images/2_90.jpg
+cp ../training_data/RENDER/bike_3/90_0_00.jpg ../sample_images/3_90.jpg
+cp ../training_data/RENDER/bike_5/90_0_00.jpg ../sample_images/5_90.jpg
+cp ../training_data/RENDER/bike_7/90_0_00.jpg ../sample_images/7_90.jpg
+cp ../training_data/RENDER/bike_9/90_0_00.jpg ../sample_images/9_90.jpg
+
+cp ../training_data/MASK/bike_1/45_0_00.png ../sample_images/1_45_mask.png
+cp ../training_data/MASK/bike_2/45_0_00.png ../sample_images/2_45_mask.png
+cp ../training_data/MASK/bike_3/45_0_00.png ../sample_images/3_45_mask.png
+cp ../training_data/MASK/bike_5/45_0_00.png ../sample_images/5_45_mask.png
+cp ../training_data/MASK/bike_7/45_0_00.png ../sample_images/7_45_mask.png
+cp ../training_data/MASK/bike_9/45_0_00.png ../sample_images/9_45_mask.png
+
+cp ../training_data/MASK/bike_1/90_0_00.png ../sample_images/1_90_mask.png
+cp ../training_data/MASK/bike_2/90_0_00.png ../sample_images/2_90_mask.png
+cp ../training_data/MASK/bike_3/90_0_00.png ../sample_images/3_90_mask.png
+cp ../training_data/MASK/bike_5/90_0_00.png ../sample_images/5_90_mask.png
+cp ../training_data/MASK/bike_7/90_0_00.png ../sample_images/7_90_mask.png
+cp ../training_data/MASK/bike_9/90_0_00.png ../sample_images/9_90_mask.png
+
